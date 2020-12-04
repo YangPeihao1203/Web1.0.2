53794417c91584f2d7ee76547a9b9e8c1e66964b
YangPeihao <392403387@qq.com>
2020-12-04 16:18:24 +0800

    Auto commit.

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,13 @@
 <template>
   <div style="position: relative; min-height: 100vh">
-      <v-app-bar>
-          
+    <!-- v-app-bar标签是vuetify的内容，我们不用管 -->
+    <v-app-bar flat elevation="0" color="white" style="z-index: 1000">
+       <v-app-bar-nav-icon
+       id="button-menu"
+       @click="drawer=true"></v-app-bar-nav-icon>
+       <v-toolbar-title class="text-h6">关于</v-toolbar-title>
+        <v-spacer></v-spacer>
       </v-app-bar>
   </div>
+  
 </template>
